0cdf5c7771fe65e4f98c89174d5942ca59cd06d5
GAlbuquerque <gtovaralbuquerque@gmail.com>
2018-11-19 21:33:29 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,18 @@ using LinearAlgebra, Statistics, Compat, ForwardDiff
 
 @testset "NewtonsMethod.jl" begin
 
-f1(x) = (x - 1)^3
-f1_prime(x) = 3*((x-1)^2)
-@test newtonroot(f1, f1_prime).root ≈ 0.9999998643434097
+f1(x) = x^2+1 #This function has no root, it should return nothing
+f1_prime(x) = 2*x 
+  
+@test newtonroot(f1, f1_prime,x₀ = 0).root == nothing
+@test newtonroot(f1, x₀ = 0).root == nothing
+ 
+  
+f2(x) = (x - 1)^3
+f2_prime(x) = 3*((x-1)^2)
+  
+  
+  
+@test newtonroot(f2, f2_prime,x₀ = 0).root ≈ 0.9999998643434097
 
 end

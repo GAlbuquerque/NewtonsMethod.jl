d60223e4f4a0b57f3a3b98861d1438eb59312f27
GAlbuquerque <gtovaralbuquerque@gmail.com>
2018-11-20 10:02:00 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,16 +17,17 @@ f1_prime(x) = 2*x
 f2(x) = (x - 1)^3
 f2_prime(x) = 3*((x-1)^2)
   
-f3(x) = log(x-2)
-f3_prime(x) = 1/(x-2)
+f3(x) = 4*x+5
+f3_prime(x) = 4
   
 f4(x) = (x-5)^x  
+f4_prime(x) =  (x-5)^x * log(x-5)
   
 @test newtonroot(f2, f2_prime,x₀ = 0).root ≈ 0.9999998643434097
 @test newtonroot(f2, x₀ = 0).root ≈ 0.9999998643434097
 
-@test newtonroot(f3, f3_prime,x₀ = 0).root ≈ 3
-@test newtonroot(f3, x₀ = 0).root ≈ 3
+@test newtonroot(f3, f3_prime,x₀ = 0).root ≈ -1.25
+@test newtonroot(f3, x₀ = 0).root ≈ -1.25
 
 @test newtonroot(f4, f4_prime,x₀ = 0).root ≈ 5
 @test newtonroot(f4, x₀ = 0).root ≈ 5

28f9fe6b234c7a9eb4083c03b5d34b3f23af215d
GAlbuquerque <gtovaralbuquerque@gmail.com>
2018-11-19 20:20:30 -0800

    Update NewtonsMethod.jl

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -6,27 +6,23 @@ activate_github("QuantEcon/QuantEconLecturePackages", tag = "v0.9.0") # activate
 using ForwardDiff
 
 #Newton Method:
-function newtonroot(f, f_prime; x_0, tol = 1E-7, maxiter = 1000r)
-        @assert f_prime != 0
+function newtonroot(f, f_prime; x_0, tol = 1E-7, maxiter = 1000)
         x = x_0
         error = Inf
         iter = 1
         while iter < max_iter && error > tol
+                 if f′(x_old) ≈ 0.0
+                 return (root = nothing, normdiff = nothing, iter = nothing)
+        end
             error = abs(- (f(x)/f_prime(x)))
             x = x - (f(x)/f_prime(x))
             iter = iter + 1
-        end
-  
-  error-----------------------------------
-  
+        end  
 if iter == maxiter+1
         return (root = nothing, normdiff = nothing, iter = nothing)
     else
         return (root = x_old, normdiff = normdiff, iter = iter)
     end
-end
--------------------------------------------
-(x,iter)
 end
 
 # Applying auto-differentiation
@@ -37,9 +33,6 @@ newtonroot(f; x₀, tol = 1E-7, maxiter = 1000) = newtonroot(f, D(f), x₀=x₀,
 
 # Exporting the function
 export newtonroot
-
-end # module
-
 
 end # module
 

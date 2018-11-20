c8e413256ce60022da8f26eb3979b1da113e4984
GAlbuquerque <gtovaralbuquerque@gmail.com>
2018-11-19 21:07:20 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using NewtonsMethod
 using Test
 
+using LinearAlgebra, Statistics, Compat, ForwardDiff
+
 @testset "NewtonsMethod.jl" begin
-    # Write your own tests here.
+
+f1(x) = (x - 1)^3
+f1_prime(x) = 3*((x-1)^2)
+@test newtonroot(f1, f1_prime).root ≈ 0.9999998643434097
+
 end
